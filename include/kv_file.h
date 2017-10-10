9eeb029b507d507c315cc853529ca90d29b97033
Darrell Wright <dwright-git@dawdevel.ca>
2017-10-10 00:42:39 -0400

    Added formatting and removed dep on boost::string_view

--- include/kv_file.h
+++ include/kv_file.h
@@ -1,16 +1,16 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 Darrell Wright
+// Copyright (c) 2016-2017 Darrell Wright
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files( the "Software" ), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
+// of this software and associated documentation files( the "Software" ), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and / or
+// sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -22,39 +22,41 @@
 
 #pragma once
 
-#include <boost/utility/string_view.hpp>
-#include <boost/optional.hpp>
 #include <string>
 #include <vector>
+
+#include <daw/daw_string_view.h>
 
 namespace daw {
 	struct kv_pair {
 		std::string key;
 		std::string value;
-		kv_pair( );
-		kv_pair( std::string k, std::string p );
+		kv_pair( std::string k, std::string p ) noexcept;
 		~kv_pair( );
+		kv_pair( ) = delete;
 		kv_pair( kv_pair const & ) = default;
-		kv_pair( kv_pair && ) = default;
-		kv_pair & operator=( kv_pair const & ) = default;
-		kv_pair & operator=( kv_pair && ) = default;
-	};	// kv
+		kv_pair( kv_pair && ) noexcept = default;
+		kv_pair &operator=( kv_pair const & ) = default;
+		kv_pair &operator=( kv_pair && ) noexcept = default;
+	}; // kv
 
-	kv_pair parse_kv_string( boost::string_view line );
+	kv_pair parse_kv_string( daw::string_view line );
 
 	struct kv_file {
 		using values_type = std::vector<kv_pair>;
 		using iterator = values_type::iterator;
 		using const_iterator = values_type::const_iterator;
+
 	private:
 		std::vector<kv_pair> m_values;
+
 	public:
 		kv_file( );
-		kv_file( boost::string_view file_name );
+		kv_file( daw::string_view file_name );
 
-		values_type const & data( ) const;
-		values_type & data( );
-		
+		values_type const &data( ) const;
+		values_type &data( );
+
 		iterator begin( );
 		const_iterator begin( ) const;
 		const_iterator cbegin( ) const;
@@ -63,8 +65,8 @@ namespace daw {
 		const_iterator end( ) const;
 		const_iterator cend( ) const;
 		std::string to_string( ) const;
-		void to_file( boost::string_view file_name ) const;
-		kv_file & add( std::string key, std::string value );
-	};	// kv_file
-}    // namespace daw
+		void to_file( daw::string_view file_name ) const;
+		kv_file &add( std::string key, std::string value );
+	}; // kv_file
+} // namespace daw
 
--- src/kv_file.cpp
+++ src/kv_file.cpp
@@ -1,16 +1,16 @@
 // The MIT License (MIT)
 //
-// Copyright (c) 2016 Darrell Wright
+// Copyright (c) 2016-2017 Darrell Wright
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files( the "Software" ), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
+// of this software and associated documentation files( the "Software" ), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and / or
+// sell copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -20,48 +20,40 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-#include <boost/utility/string_view.hpp>
-#include <boost/algorithm/string/trim.hpp>
 #include <fstream>
 #include <sstream>
 #include <string>
 
 #include <daw/daw_bounded_stack.h>
+#include <daw/daw_parser_helper_sv.h>
+#include <daw/daw_string_view.h>
 
 #include "kv_file.h"
 
 namespace daw {
-	kv_pair::kv_pair( ): 
-			key{ },
-			value{ } { }
+	kv_pair::kv_pair( std::string k, std::string v ) noexcept : key{std::move( k )}, value{std::move( v )} {}
 
-	kv_pair::kv_pair( std::string k, std::string v ):
-			key{ std::move( k ) },
-			value{ std::move( v ) } { }
+	kv_pair::~kv_pair( ) = default; 
 
-	kv_pair::~kv_pair( ) { }
-
-	kv_pair parse_kv_string( boost::string_view line ) {
-		kv_pair result;
+	kv_pair parse_kv_string( daw::string_view line ) {
 		daw::bounded_stack_t<char, 2> ch_stack;
 		bool in_quote = false;
-		for( size_t pos=0; pos<line.size( ); ++pos ) {
-			switch( line[pos] ) {
-				case '\\':
-					ch_stack.push_back( '\\' );
+		auto first = line.begin( );
+		while( !line.empty( ) ) {
+			switch( line.front( ) ) {
+			case '\\':
+			case '\"':
+				ch_stack.push_back( line.pop_front( ) );
 				break;
-				case '\"': {
-					ch_stack.push_back( '\"' );
-					break;
+			case '=':
+				if( !in_quote ) {
+					return {
+					  daw::parser::trim( daw::make_string_view_it( first, line.begin( ) ) ).to_string( ),
+					  daw::parser::trim( daw::make_string_view_it( std::next( line.begin( ) ), line.end( ) ) ).to_string( )};
 				}
-				case '=': 
-					if( !in_quote ) {
-						auto sep = std::next( line.begin( ), static_cast<std::iterator_traits<decltype(line.begin( ))>::difference_type>(pos) );
-						result.key = boost::trim_copy( std::string{ line.begin( ), sep } );
-						result.value = boost::trim_copy( std::string{ std::next( sep ), line.end( ) } );
-						return result;
-					}
-					break;
+			default:
+				line.remove_prefix( );
+				break;
 			}
 			if( !ch_stack.empty( ) ) {
 				if( ch_stack.front( ) == '\\' ) {
@@ -74,41 +66,36 @@ namespace daw {
 				}
 			}
 		}
-		std::stringstream ss;
-		ss << "Could not find unquoted = symbol to split line on: '" << line.data( ) << "'";
-		throw std::runtime_error( ss.str( ) ); 
+		daw::exception::daw_throw( std::string{"Could not find unquoted = symbol to split line on: '"} + line + "'" );
 	}
 
-	kv_file::kv_file( boost::string_view file_name ): m_values{ } {
+	kv_file::kv_file( daw::string_view file_name ) : m_values{} {
 		std::ifstream in_file;
 		in_file.open( file_name.data( ), std::ios::binary );
 
-		if( !in_file ) {
-			std::stringstream ss;
-			ss << "Unable to open file '" << file_name.data( ) << "'";
-			throw std::runtime_error( ss.str( ) ); 
-		}
+		daw::exception::daw_throw_on_false( in_file, "Unable to open file '{0}'", file_name.to_string( ) );
 
 		in_file >> std::noskipws;
 		std::string line;
 
 		while( std::getline( in_file, line ).good( ) ) {
-			line = boost::trim_copy( line );
+			daw::string_view sv{line};
+			sv = daw::parser::trim( sv );
 			if( line.empty( ) || line.front( ) == '#' ) {
 				continue;
 			}
-			m_values.push_back( parse_kv_string( line ) );	
+			m_values.push_back( parse_kv_string( line ) );
 		}
 	}
 
-	kv_file::values_type const & kv_file::data( ) const {
+	kv_file::values_type const &kv_file::data( ) const {
 		return m_values;
 	}
-	
-	kv_file::values_type & kv_file::data( ) {
+
+	kv_file::values_type &kv_file::data( ) {
 		return m_values;
 	}
-	
+
 	kv_file::iterator kv_file::begin( ) {
 		return m_values.begin( );
 	}
@@ -135,29 +122,27 @@ namespace daw {
 
 	std::string kv_file::to_string( ) const {
 		std::stringstream ss;
-		for( auto const & kv: m_values ) {
+		for( auto const &kv : m_values ) {
 			ss << kv.key << "=" << kv.value << '\n';
 		}
 		return ss.str( );
 	}
 
-	void kv_file::to_file( boost::string_view file_name ) const {
+	void kv_file::to_file( daw::string_view file_name ) const {
 		std::ofstream out_file;
 		out_file.open( file_name.data( ), std::ios::binary | std::ios::trunc );
-		if( !out_file ) {
-			std::stringstream ss;
-			ss << "Could not open file '" << file_name.data( ) << "'";
-			throw std::runtime_error( ss.str( ) );
-		}
-		for( auto const & kv: m_values ) {
+
+		daw::exception::daw_throw_on_false( out_file, "Could not open file '{0}'", file_name.to_string( ) );
+
+		for( auto const &kv : m_values ) {
 			out_file << kv.key << "=" << kv.value << '\n';
 		}
 	}
-	kv_file::kv_file( ): m_values{ } { }
+	kv_file::kv_file( ) : m_values{} {}
 
-	kv_file & kv_file::add( std::string key, std::string value ) {
+	kv_file &kv_file::add( std::string key, std::string value ) {
 		m_values.emplace_back( std::move( key ), std::move( value ) );
 		return *this;
 	}
-}    // namespace daw
+} // namespace daw
 
